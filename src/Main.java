cc5cce3e626224f256773e57bfecd870833c965e
mabumhana <m.abumhana@gmail.com>
2020-04-17 20:27:29 +0300

    calling getName() method from user1_branch and printing it

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         DevOps obj =new DevOps("Mohammad Abu Mhana");
         obj.print();
         obj.setName("mabumhana");
+        String name = obj.getName();
+        System.out.println("user1_branch: \n Name : "+name);
     }
 }
