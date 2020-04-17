4aad8073bcd2c6d5bf986a71c0668a33e8757fc2
mabumhana <m.abumhana@gmail.com>
2020-04-17 21:32:04 +0300

    declaring and printing x in user2_branch

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         obj.setName("mabumhana");
         String name = obj.getName();
         System.out.println("user1_branch: \n Name : "+name);
+        int xuser2_branch = 5;
+        System.out.println("x from user2_branch"+xuser2_branch);
     }
 }
