11449dcad16ee12c5d237387cbaec9af21c56f54
mabumhana <m.abumhana@gmail.com>
2020-04-17 20:07:33 +0300

    ver1.0

---
+++ src/DevOps.java
@@ -0,0 +1,18 @@
+public class DevOps {
+    private String name;
+
+    public DevOps(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void print(){
+        System.out.println("Name : "+name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String []args){
+        DevOps obj =new DevOps("Mohammad Abu Mhana");
+        obj.print();
+    }
+}
